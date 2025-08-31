d93c1d1f84292207af4d2b783837d24598c5520e
rakesh <rakeshcodes2005@gmail.com>
2025-08-31 08:39:12 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { SpotlightNewDemo } from "@/components/ui/spotlight";
+
+export default function Home() {
+  return (
+    <div>
+      <SpotlightNewDemo/>
+    </div>
+  );
+}
