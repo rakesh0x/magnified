54e807d771280b884763214db476c3b430f2983d
rakesh <rakeshcodes2005@gmail.com>
2025-09-02 20:15:33 +0530

    input bar

--- components/magicui/shining-border.tsx
+++ components/magicui/shining-border.tsx
@@ -1,8 +1,5 @@
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ShineBorder } from "@/components/magicui/shine-border";
@@ -11,7 +8,7 @@ export function ShineBorderDemo() {
   return (
     <div className="relative max-w-xl w-full bg-[#18181b]/80 rounded-2xl h-40 border border-[#3a3a3c] shadow-lg p-4">
       <Input
-        className="relative z-10 w-full bg-transparent text-white placeholder:text-[#b1b1b1] rounded-2xl px-4 py-6 text-lg border-none focus:outline-none"
+        className="relative z-10 w-full bg-transparent text-white placeholder:text-[#b1b1b1] rounded-2xl px-4 py-6 text-lg border-none focus:outline-none focus:ring-0"
         placeholder="Type your prompt with your image, rest we will take care of .."
       />
       <ShineBorder
