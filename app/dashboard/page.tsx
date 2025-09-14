e34af2b7b4e485122b60fa7a30ab06df9e120163
rakesh <rakeshcodes2005@gmail.com>
2025-09-14 14:38:48 +0530

    dashboard-design

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import ImageTrainerUI from "@/components/ui/collapsible-prompt-window"
+
+export default function Dashboard() {
+    return (
+        <div>
+            <ImageTrainerUI/>
+        </div>
+    )
+}
