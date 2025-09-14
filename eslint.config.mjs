7eca03f452131e90e8777a6cce776879852ff6e1
rakesh <rakeshcodes2005@gmail.com>
2025-09-14 18:17:39 +0530

    type-fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,7 +22,10 @@ const eslintConfig = [
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
-      '@typescript-eslint/no-empty-object-type': 'off'
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@next/next/no-img-element': 'off'
     }
   },
 ];
