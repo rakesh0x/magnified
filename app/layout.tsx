e2def0700a7af9e1947f21b21bb8cca5f94a6a51
rakesh <rakeshcodes2005@gmail.com>
2025-09-17 20:17:43 +0530

    auth-clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,14 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,14 +30,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="/magnified.png" />
-      </head>
-      <body className={` bg-[#0a0a0a] ${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Analytics/>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <link rel="icon" href="/magnified.png" />
+        </head>
+        <body className={` bg-[#0a0a0a] ${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <Analytics/>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+      
   );
 }
--- components/ui/onboarding.tsx
+++ components/ui/onboarding.tsx
@@ -1,9 +1,12 @@
 "use client" 
 import { Button, buttonVariants } from "./button"
+import { SignInButton } from '@clerk/nextjs'
 
 export  function Auth() {
     return (
+
+        <SignInButton mode="modal">
             <Button>Get Started</Button>
-        
+        </SignInButton>
     )
 }
